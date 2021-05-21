4cfdd41717402b482784b5e34f18a12390fa186e
Chandler Mathews <ctmathews2@usfca.edu>
2021-05-20 18:07:44 -0700

    Initial Commit

---
+++ PPL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PPL
+//
+//  Created by Chandler Mathews on 5/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PPL/PPLApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPLApp.swift
+//  PPL
+//
+//  Created by Chandler Mathews on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PPLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
