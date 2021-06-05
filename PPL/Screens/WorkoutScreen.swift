682b0dab85b50d757d83af5efe2832f82683c017
Chandler Mathews <ctmathews2@usfca.edu>
2021-06-05 11:27:58 -0700

    Added popup form for setItems

--- PPL/Screens/WorkoutScreen.swift
+++ PPL/Screens/WorkoutScreen.swift
@@ -7,6 +7,66 @@
 
 import SwiftUI
 
+struct SheetView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        Button("Press to dismiss") {
+            presentationMode.wrappedValue.dismiss()
+        }
+        .font(.title)
+        .padding()
+        .background(Color.black)
+    }
+}
+
+struct SetItem: View {
+    var liftItem : ExcerciseItem
+    var repsCompleted : Int = 0
+    var weight : Int = 0
+    @State var showPopUp : Bool = false
+    @EnvironmentObject var helper : Helper
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Set 1")
+                    Spacer()
+                    Text("Weight: \(weight)")
+                    Spacer()
+                    Text("Reps: \(repsCompleted) (10)")
+                }
+                Button("Modal") {
+                    self.isModal = true
+                }.sheet(isPresented: $isModal, content: {
+                    self.modal
+                })
+                Button("Show Sheet") {
+                    showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SheetView()
+                }
+                Button(action: {
+                    //self.showPopUp = true
+                    self.helper.changeField(text: liftItem.name)
+                }, label: {
+                    Text("Show custom pop up")
+                })
+            }
+        }
+    }
+    
+    @State var isModal: Bool = false
+    @State private var showingSheet = false
+
+
+    var modal: some View {
+        Text("Modal")
+    }
+}
+
 struct ExcerciseItem: Identifiable, Hashable {
     var id = UUID()
     var name: String
@@ -23,20 +83,57 @@ struct ExcerciseCellView: View {
     var body: some View {
         HStack {
             content
-            Spacer()
+            //Spacer()
         }
-        .contentShape(Rectangle())
     }
     
     private var content: some View {
         VStack(alignment: .leading) {
-            Text(excercise.name).font(.headline)
+            HStack {
+                Text(excercise.name).font(.largeTitle)
+                Spacer()
+                Text("\(excercise.setsCompleted)/\(excercise.setsTotal)").font(.largeTitle)
+            }
+            .contentShape(Rectangle())
+            //.frame(width: UIScreen.screenWidth, height: isExpanded == true ? 200 : 150, alignment: .center)
+            //.border( /*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+            .frame( height: 100, alignment: .center)
+            .background(Color.blue)
+            .overlay(Rectangle().frame(width: nil, height: 1, alignment: .top).foregroundColor(Color.black), alignment: .bottom)
+            
+            
             
             if isExpanded{
                 // This is where I would put the sets object
                 VStack(alignment: .leading) {
-                    Text("\(excercise.setsCompleted)")
-                    Text("\(excercise.setsTotal)")
+                    // For loops for sets item array?
+//                    Text("\(excercise.setsCompleted)")
+//                    Text("\(excercise.setsTotal)")
+                    HStack {
+                        Text("Set 1")
+                        Spacer()
+                        Text("Add Reps:")
+                        Spacer()
+                        Text("Add Weight:")
+                    }
+                    .background(Color.red)
+                    HStack {
+                        Text("Set 2")
+                        Spacer()
+                        Text("Add Reps:")
+                        Spacer()
+                        Text("Add Weight:")
+                    }
+                    .background(Color.red)
+                    HStack {
+                        Text("Set 3")
+                        Spacer()
+                        Text("Add Reps:")
+                        Spacer()
+                        Text("Add Weight:")
+                    }
+                    .background(Color.red)
+                    SetItem(liftItem: excercise)
                 }
             }
         }
@@ -73,8 +170,8 @@ struct ListRowModifier: ViewModifier {
     func body(content: Content) -> some View {
         Group {
             content
-            Divider()
-        }.offset(x: 20)
+            //Divider()
+        }.offset(x: 0)
     }
 }
 
@@ -87,12 +184,47 @@ let ExcerciseItems =
         ExcerciseItem(name: "Tricep Pulldown", setsCompleted: 0, setsTotal: 3)
     ]
 
+class Helper: ObservableObject {
+
+    @Published var myGlobbal : Bool = false
+    @Published var globalText : String = "hey"
+
+    func changeField(text: String) {
+        myGlobbal.toggle()
+        globalText = text
+    }
+}
+
 struct WorkoutScreen: View {
-    
+    @EnvironmentObject var helper : Helper
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            ExcerciseListView(excercises: ExcerciseItems)
+        ZStack {
+            VStack {
+                Text("Push")
+                    .frame(maxWidth: .infinity, maxHeight: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.top, 20)
+                    .background(Color.green)
+                ExcerciseListView(excercises: ExcerciseItems)
+            }
+            .ignoresSafeArea()
+            if self.helper.myGlobbal {
+                ZStack {
+                    Color.white
+                    VStack {
+                        Text("Custom Pop Up")
+                        Text("\(self.helper.globalText)")
+                        Spacer()
+                        Button(action: {
+                            //self.showPopUp = false
+                            self.helper.changeField(text: "Closed")
+                        }, label: {
+                            Text("Close")
+                        })
+                    }.padding()
+                }
+                .frame(width: 300, height: 200)
+                .cornerRadius(20).shadow(radius: 20)
+            }
         }
     }
 }
@@ -100,5 +232,7 @@ struct WorkoutScreen: View {
 struct WorkoutScreen_Previews: PreviewProvider {
     static var previews: some View {
         WorkoutScreen()
+            .environmentObject(Helper())
+
     }
 }
